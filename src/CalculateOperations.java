7d235431397fe002ded3ce441168e8a4254502d4
tuncalie <tuncalie@yahoo.com>
2015-11-10 16:51:12 -0700

    added implementation of operations

--- src/CalculateOperations.java
+++ src/CalculateOperations.java
@@ -2,20 +2,20 @@
 public class CalculateOperations {
 //fill in the correct return values.
 	int add(int a, int b){
-		return 0;
+		return a+b;
 	}
 	
 	int subtract (int a, int b){
-		return 0;
+		return a-b;
 	}
 	
 	int multiply(int a, int b){
-		return 0;
+		return a*b;
 		
 	}
 	
 	int divide(int a, int b){
-		return 0;
+		return a/b;
 	}
 	
 	
